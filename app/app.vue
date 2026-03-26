5304c4d11eed2d67bf4b4cb0e3a6f33d98dbf8f9
MKvaratshelia <kvaratsheliamihail@yandex.ru>
2026-03-26 19:24:54 +0300

    add styles, actionButton, error page

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "@/assets/styles/main.css";
+</script>
 
 <template>
     <div>
         <NuxtPage />
     </div>
 </template>
+
+<style scoped></style>
